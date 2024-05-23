58a9caf41787afeb03808da5b574ebd8e9f79956
ausboss <102309304+ausboss@users.noreply.github.com>
2024-05-23 16:09:43 -0500

    Update Ped.cpp

--- src/util/Ped.cpp
+++ src/util/Ped.cpp
@@ -4,6 +4,38 @@
 
 namespace YimMenu::Peds
 {
+	void MarkPedAsCompanion(Ped ped)
+	{
+		Hash playerGroup = GAMEPLAY::GET_HASH_KEY((char*)"PLAYER");
+		Hash companionGroup;
+
+		PED::ADD_RELATIONSHIP_GROUP((char*)"_CHAOS_COMPANION", &companionGroup);
+		PED::SET_RELATIONSHIP_BETWEEN_GROUPS(0, companionGroup, playerGroup);
+		PED::SET_RELATIONSHIP_BETWEEN_GROUPS(0, playerGroup, companionGroup);
+
+		PED::SET_PED_RELATIONSHIP_GROUP_HASH(ped, companionGroup);
+
+		PED::SET_PED_AS_GROUP_MEMBER(ped, PLAYER::GET_PLAYER_GROUP(PLAYER::PLAYER_ID()));
+
+		/** BF_CanFightArmedPedsWhenNotArmed */
+		PED::SET_PED_COMBAT_ATTRIBUTES(ped, 5, true);
+		/** BF_AlwaysFight */
+		PED::SET_PED_COMBAT_ATTRIBUTES(ped, 46, true);
+
+		static Hash blipHash = GET_HASH("BLIP_STYLE_FRIENDLY");
+
+		/** BLIP_ADD_FOR_ENTITY */
+		Blip blip = RADAR::_0x23F74C2FDA6E7C61(blipHash, ped);
+
+		static Hash blipModifier = GET_HASH("BLIP_MODIFIER_MP_COLOR_1");
+
+		/** BLIP_ADD_MODIFIER */
+		RADAR::_0x662D364ABF16DE2F(blip, blipModifier);
+
+		/** Allow talk */
+		PED::SET_PED_CONFIG_FLAG(ped, 130, 0);
+	}
+
 	int SpawnPed(std::string model_name, Vector3 coords, float heading, bool blockNewPedMovement, bool spawnDead, bool invincible, bool invisible, int scale, bool followPlayer, bool noFleeing, bool isArmed, bool isDrunk, bool ifBodyguard)
 	{
 		Hash model = Joaat(model_name.c_str());
@@ -38,9 +70,7 @@ namespace YimMenu::Peds
 			PED::_SET_PED_DRUNKNESS(ped, true, 1.0f);
 		}
 
-
 		if (isArmed)
-		// give the ped the same weapon as the player
 		{
 			Hash weapon;
 			if (WEAPON::GET_CURRENT_PED_WEAPON(YimMenu::Self::PlayerPed, &weapon, true, 0, true))
@@ -49,67 +79,27 @@ namespace YimMenu::Peds
 			}
 		}
 
-
 		if (followPlayer)
 		{
-			// Create a group and set the player as the leader
 			int groupID = PED::CREATE_GROUP(0);
 			PED::SET_PED_AS_GROUP_LEADER(YimMenu::Self::PlayerPed, groupID, 0);
 			PED::SET_PED_AS_GROUP_MEMBER(ped, groupID);
 			PED::SET_PED_CAN_TELEPORT_TO_GROUP_LEADER(ped, groupID, true);
-			PED::SET_GROUP_SEPARATION_RANGE(groupID, 999999.9f); // Very high range to prevent separation
+			PED::SET_GROUP_SEPARATION_RANGE(groupID, 999999.9f);
 
-			// Set group formation and spacing
-			PED::SET_GROUP_FORMATION(groupID, 0); // Default formation
+			PED::SET_GROUP_FORMATION(groupID, 0);
 			PED::SET_GROUP_FORMATION_SPACING(groupID, 1.0f, 1.0f, 1.0f);
 
-			// Make the ped follow the player and engage in combat
 			TASK::TASK_FOLLOW_TO_OFFSET_OF_ENTITY(ped, YimMenu::Self::PlayerPed, 0.0f, 0.0f, 0.0f, 1.0f, -1, 1.0f, true, false, false, true, false, true);
 			PED::SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, true);
 		}
 
-		// If ifBodyguard toggle is enabled
 		if (ifBodyguard)
 		{
-			// Give the ped the same weapon as the player
-			Hash weapon;
-			if (WEAPON::GET_CURRENT_PED_WEAPON(YimMenu::Self::PlayerPed, &weapon, true, 0, true))
-			{
-				WEAPON::GIVE_WEAPON_TO_PED(ped, weapon, 1000, true, true, 0, false, 0.5f, 1.0f, 0, 0, 0.5f, 0);
-			}
-
-			// Set combat attributes to make the ped defend the player
-			PED::SET_PED_COMBAT_ATTRIBUTES(ped, 0, true);  // Enable combat attributes
-			PED::SET_PED_COMBAT_ATTRIBUTES(ped, 1, true);  // Enable fighting
-			PED::SET_PED_COMBAT_ATTRIBUTES(ped, 46, true); // Enable to attack threats
-			PED::SET_PED_COMBAT_MOVEMENT(ped, 2);          // Offensive combat movement
-			PED::SET_PED_COMBAT_RANGE(ped, 1);             // Close range
-			PED::SET_PED_AS_GROUP_MEMBER(ped, groupID);
-			PED::SET_PED_CONFIG_FLAG(ped, 279, true);      // Never leaves group
-			PED::REGISTER_TARGET(ped, Global_35, true);    // Register player as target for defense
-			PED::SET_PED_ACCURACY(ped, 60);                // Set accuracy
-			PED::SET_PED_COMBAT_ATTRIBUTES(ped, 58, true); // Use cover
-
-			// Make the ped attack anyone who attacks the player
-			PED::REGISTER_TARGET(ped, YimMenu::Self::PlayerPed, true);
-			PED::SET_PED_FLEE_ATTRIBUTES(ped, 0, false);   // Do not flee
-			PED::SET_PED_COMBAT_ATTRIBUTES(ped, 46, true); // Always fight back
-
-			// Additional necessary combat settings
-			PED::SET_PED_RELATIONSHIP_GROUP_HASH(ped, Joaat("REL_PLAYER_ALLY"));
-			int blip = MAP::BLIP_ADD_FOR_ENTITY(Joaat("BLIP_STYLE_ENEMY"), ped);
-			MAP::BLIP_ADD_MODIFIER(blip, Joaat("BLIP_MODIFIER_ENEMY_DISAPPEARING"));
-			PED::SET_PED_DEFENSIVE_AREA_VOLUME(ped, iLocal_56, false, false, false);
-			PED::_0x815C0074A1BC0D93(ped, 0);
-			PED::_0x1E017404784AA6A3(ped, Joaat("PLAYER"));
-			PED::_SET_PED_COMBAT_BEHAVIOUR(ped, 2018638702);
-			int iVar5 = 0;
-			func_33(&iVar5, 15);
-			func_33(&iVar5, 16);
-			COMPANION::_0xDEB369F6AD168C58(ped, iVar5);
+			MarkPedAsCompanion(ped);
 		}
 
 		STREAMING::SET_MODEL_AS_NO_LONGER_NEEDED(model);
 		return ped;
-	};
+	}
 }
