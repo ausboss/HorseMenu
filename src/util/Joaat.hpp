454907da2c7e863ccbc1e50c8ee73b07cc44d55a
ausboss <102309304+ausboss@users.noreply.github.com>
2024-05-22 19:06:28 -0500

    updating joaat

--- src/util/Joaat.hpp
+++ src/util/Joaat.hpp
@@ -4,29 +4,43 @@
 
 namespace YimMenu
 {
-	using joaat_t = std::uint32_t;
+    using joaat_t = std::uint32_t;
 
-	inline constexpr char ToLower(char c)
-	{
-		return c >= 'A' && c <= 'Z' ? c | 1 << 5 : c;
-	}
+    inline constexpr char ToLower(char c)
+    {
+        return c >= 'A' && c <= 'Z' ? c | 1 << 5 : c;
+    }
 
-	extern constexpr joaat_t Joaat(const std::string_view str);
-	inline consteval joaat_t operator""_J(const char* s, std::size_t n)
-	{
-		joaat_t result = 0;
+    constexpr joaat_t Joaat(const std::string_view str)
+    {
+        joaat_t hash = 0;
+        for (auto c : str)
+        {
+            hash += ToLower(c);
+            hash += (hash << 10);
+            hash ^= (hash >> 6);
+        }
+        hash += (hash << 3);
+        hash ^= (hash >> 11);
+        hash += (hash << 15);
+        return hash;
+    }
 
-		for (std::size_t i = 0; i < n; i++)
-		{
-			result += ToLower(s[i]);
-			result += (result << 10);
-			result ^= (result >> 6);
-		}
+    inline consteval joaat_t operator""_J(const char* s, std::size_t n)
+    {
+        joaat_t result = 0;
 
-		result += (result << 3);
-		result ^= (result >> 11);
-		result += (result << 15);
+        for (std::size_t i = 0; i < n; i++)
+        {
+            result += ToLower(s[i]);
+            result += (result << 10);
+            result ^= (result >> 6);
+        }
 
-		return result;
-	}
+        result += (result << 3);
+        result ^= (result >> 11);
+        result += (result << 15);
+
+        return result;
+    }
 }
